c4017c921ef69053ef007b05f9192de20c08c4b0
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-06-01 16:04:54 -0500

    remove port mapping

--- main.groovy
+++ main.groovy
@@ -8,7 +8,6 @@ pipeline {
                 cpu 256
                 memory 512
                 image 'jenkinsci/jnlp-slave'
-                portMappings([[containerPort: 22, hostPort: 22, protocol: 'tcp'], [containerPort: 443, hostPort: 443, protocol: 'tcp']])
             }
         }
         steps {
