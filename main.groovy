fee40a4f8aeb5f7f264b31b4f5f627f6a76da04b
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-06-01 17:04:52 -0500

    add some configs

--- main.groovy
+++ main.groovy
@@ -8,6 +8,9 @@ pipeline {
                 cpu 256
                 memory 512
                 image 'jenkins/inbound-agent'
+                logDriver 'awslogs'
+                logDriverOptions([[name: 'awslogs-group', value:'/ecs/sample-jenkins-ecs'], [name: 'awslogs-region', value: 'us-east-2'], [name: 'awslogs-stream-prefix', value: 'ecs']])
+                portMappings([[containerPort: 22, hostPort: 22, protocol: 'tcp'], [containerPort: 443, hostPort: 443, protocol: 'tcp']])
             }
         }
         steps {
