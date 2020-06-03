d8f1d401805c31211950a51a03df513ce50ccc9c
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-06-03 12:05:04 -0500

    add launchType

--- main.groovy
+++ main.groovy
@@ -11,6 +11,7 @@ pipeline {
                 logDriver 'awslogs'
                 logDriverOptions([[name: 'awslogs-group', value:'/ecs/sample-jenkins-task-definition'], [name: 'awslogs-region', value: 'us-east-2'], [name: 'awslogs-stream-prefix', value: 'ecs']])
                 portMappings([[containerPort: 22, hostPort: 22, protocol: 'tcp'], [containerPort: 443, hostPort: 443, protocol: 'tcp']])
+                launchType ''	
             }
         }
         steps {
