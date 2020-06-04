0cc261c1fff0302bd8055165c72dacd22cfe7c50
Miguel Cuartin <miguel.cuartin@avaldigitallabs.com>
2020-06-04 08:02:15 -0500

    remove launchType

--- main.groovy
+++ main.groovy
@@ -11,7 +11,6 @@ pipeline {
                 logDriver 'awslogs'
                 logDriverOptions([[name: 'awslogs-group', value:'/ecs/sample-jenkins-task-definition'], [name: 'awslogs-region', value: 'us-east-2'], [name: 'awslogs-stream-prefix', value: 'ecs']])
                 portMappings([[containerPort: 22, hostPort: 22, protocol: 'tcp'], [containerPort: 443, hostPort: 443, protocol: 'tcp']])
-                launchType 'Default Capacity Provider'	
             }
         }
         steps {
